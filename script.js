5b070ef1b58f67e0205b2ed5a5fe64723d8654dd
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-06-05 19:30:10 -0700

    added classes and id to buttons

--- script.js
+++ script.js
@@ -1,102 +1,26 @@
 $(document).ready(function () {
-    question();
+    q1();
 });
 
-function question() {
-    $('.yes').click(function () {
-        $(this).addClass('not_exist');
-        show();
-        // $('.answer').removeClass('answer');
-
+function q1() {
+    $('#yolo1').click(function () {
+        button();
+        $('#yolo').removeClass('answer');
     })
 }
 
-function show() {
-    if($('.yes').hasClass('not_exist')) {
-        $('.answer').removeClass('answer');
-    }
+function button() {
+    $(this).addClass('not_exist');
 }
 
-
-// function Decision() {
-//     // this.yes = 0;
-//     this.sure = 0;
-//     this.question = function () {
-//         // var sure = 0;
-// //         // if ($('.yes').click(function () {
-// //         // $('.answer').toggleClass("hide");
-// //         // }))
-// //         $('.yes').click(function () {
-// //             alert("YOLO");
-// //             $('h2').removeClass('answer');
-//         // })
-//         $(".yes").click(function () {
-//             $("#yolo").toggleClass("answer");
-//             this.sure += 1;
-//             return sure;
-//         });
-//         console.log("sure1", sure);
-//
-//     }
+// function button() {
+//     $('.answer').on('click', '.yes', function () {
+//         $(this).removeClass('answer');
+//     })
 // }
 
-
-
 // $(document).ready(function () {
-//     Decision();
-//     $("button").click(function () {
-//         question();
-//     });
-//     // $(".yes").click(function () {
-//     //     $("#yolo").toggleClass("answer");
-//     // })
-// });
-
-
-// $(".hint").click(function () {
-//     $("#hidden").toggleClass("hide");
-// })
-
-
-
-
-
-
-// $(document).ready(function () {
-//         question();
-//
-//     console.log("sure1", sure);
-//
-// });
-//
-// var add = (function () {
-//     var sure = 0;
-//     return function () {
-//         return sure++;
-//     }
-// })();
-//
-//
-// function question() {
-//     // var add = (function () {
-//     //     var sure = 0;
-//     //     return function () {
-//     //         return sure++;
-//     //     }
-//     // })();
-//     $(".yes").click(function () {
-//         $("#yolo").toggleClass("answer");
-//         add();
-//     });
-// }
-
-// console.log("sure1", sure);
-
-// $(document).ready(function () {
-//     var sure = 0;
-//     $(".yes").click(function () {
-//         $("#yolo").toggleClass("answer");
-//         sure++;
-//     });
-//     console.log("sure1", sure);
+//     $('.answer').on('click','button', function () {
+//         $(this).addClass('not_exist');
+//     })
 // });
