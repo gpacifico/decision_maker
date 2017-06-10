393f451121093a77aaf0367a1a8390fc5c189978
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-06-09 21:44:15 -0700

    added scoring system, additional questions, and functions for each question

--- script.js
+++ script.js
@@ -1,38 +1,77 @@
-// $(document).ready(function () {
-//     q1();
-// });
-//
-// function q1() {
-//     $('#yolo1').click(function () {
-//         button();
-//         $('#yolo').removeClass('answer');
-//     })
-// }
-//
-// function button() {
-//     $(this).addClass('not_exist');
-// }
-
-// function button() {
-//     $('.answer').on('click', '.yes', function () {
-//         $(this).removeClass('answer');
-//     })
-// }
-
-// $(document).ready(function () {
-//     $('.answer').on('click','button', function () {
-//         $(this).addClass('not_exist');
-//     })
-// });
-
-// $(document).ready(function () {
-//     $('h2').on('click', 'button', function () {
-//         $(this).removeClass('answer');
-//     })
-// });
+var points = 0;
 
 $(document).ready(function () {
-   $('h2').on('click', 'button', function () {
-       console.log($('button'));
-   })
+    question1();
+    question2();
+    question3();
+    question4();
+    question5();
+    question6();
+    choice();
 });
+
+function question1() {
+    $('#yolo1').click(function () {
+        $(this).addClass('not_exist');
+        $('#yolo2').removeClass('answer');
+        points += 12;
+        console.log('points2', points);
+    });
+}
+
+function question2() {
+    $('#health1').click(function () {
+        $(this).addClass('not_exist');
+        $('#health2').removeClass('answer');
+        points += 3;
+        console.log('points2', points);
+    });
+}
+
+function question3() {
+    $('#money1').click(function () {
+        $(this).addClass('not_exist');
+        $('#money2').removeClass('answer');
+        points += 1;
+        console.log('points2', points);
+    });
+}
+
+function question4() {
+    $('#power1').click(function () {
+        $(this).addClass('not_exist');
+        $('#power2').removeClass('answer');
+        points += 4;
+        console.log('points2', points);
+    });
+}
+
+function question5() {
+    $('#jail1').click(function () {
+        $(this).addClass('not_exist');
+        $('#jail2').removeClass('answer');
+        points += 4;
+        console.log('points2', points);
+    });
+}
+
+function question6() {
+    $('#parents1').click(function () {
+        $(this).addClass('not_exist');
+        $('#parents2').removeClass('answer');
+        points += 1;
+        console.log('points2', points);
+    });
+}
+
+function choice() {
+    $('#final_tally1').click(function () {
+        $('.questions').addClass('not_exist');
+        if (points < 12) {
+            $('#final_tally_yes').removeClass('answer');
+        }
+        else if (points >= 12) {
+            $('#final_tally_no').removeClass('answer');
+        }
+    });
+}
